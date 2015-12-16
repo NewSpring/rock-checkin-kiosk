3238d28e711ed53dad07252d14d9f74491523bd5
ben.wiley <ben.wiley@newspring.cc>
2015-12-16 15:58:01 -0500

    Move to a simplier boilerplate with customizable toolbar buttons (reload, home, etc)

--- config.js
+++
@@ -1,8 +0,0 @@
-window.config = {
-   "model_": "AppConfig",
-   "id": 1,
-   "appName": "KidSpring Check-in",
-   "homepage": "https://beta-rock.newspring.cc/attendedcheckin",
-   "enableNavBttns": false,
-   "kioskEnabled": true
-};

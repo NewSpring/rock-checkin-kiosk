1dc72793eb7920c3e08485f484215b5bd028d20f
ben.wiley <ben.wiley@newspring.cc>
2015-12-16 11:00:20 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "KidSpring Check-in",
+   "homepage": "https://beta-rock.newspring.cc/attendedcheckin",
+   "enableNavBttns": false,
+   "kioskEnabled": true
+};

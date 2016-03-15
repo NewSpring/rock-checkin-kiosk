4a33957d477f7f97633044be776778e1778b419a
briankalwat <brian@briankalwat.com>
2016-03-15 17:08:03 -0400

    updated homeUrl to checkin

--- browser.js
+++ browser.js
@@ -1,5 +1,5 @@
 onload = function() {
-  var homeUrl = "https://rock.newspring.cc/attendedcheckin";
+  var homeUrl = "https://checkin.newspring.cc/attendedcheckin";
   var currentUrl = "";
   var webview = document.querySelector("webview");
   var indicator = document.querySelector("#url-indicator");

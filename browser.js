669d4da139e9d99f55748d8f20445cf63a664bb4
briankalwat <brian@briankalwat.com>
2016-03-15 17:05:23 -0400

    added battery status

--- browser.js
+++ browser.js
@@ -4,6 +4,8 @@ onload = function() {
   var webview = document.querySelector("webview");
   var indicator = document.querySelector("#url-indicator");
   var versionIndicator = document.querySelector("#version-indicator");
+  var batteryLevel = document.querySelector("#battery-status");
+  var chargeStatus = document.querySelector("#charge-status");
 
   document.querySelector("#home").onclick = function() {
     webview.stop();
@@ -71,4 +73,49 @@ onload = function() {
   webview.src = homeUrl;
   versionIndicator.innerHTML = "v" + chrome.runtime.getManifest().version;
   updateClock();
+
+  navigator.getBattery().then(function(battery) {
+    // Update Battery Info on Initial Load
+    updateChargeInfo();
+    updateLevelInfo();
+
+    // Event Listeners for Charge & Level Changes
+    battery.addEventListener('chargingchange', function(){
+      updateChargeInfo();
+      updateLevelInfo(); // Remove This
+    });
+
+    battery.addEventListener('levelchange', function(){
+      updateLevelInfo();
+    });
+
+    // Update Battery Charge Status
+    function updateChargeInfo(){
+      if (battery.charging) {
+        chargeStatus.classList.add('fa');
+        chargeStatus.classList.add('fa-bolt');
+      } else {
+        chargeStatus.classList.remove('fa');
+        chargeStatus.classList.remove('fa-bolt');
+      }
+    }
+
+    // Update Battery Level
+    function updateLevelInfo(){
+      batteryLevel.classList.remove('fa-battery-0','fa-battery-1','fa-battery-2','fa-battery-3');
+
+      if (battery.level < .20) {
+        batteryLevel.classList.add('fa-battery-0');
+      } else if (battery.level >= .20 && battery.level < .40) {
+        batteryLevel.classList.add('fa-battery-1');
+      } else if (battery.level >= .40 && battery.level < .60) {
+        batteryLevel.classList.add('fa-battery-2');
+      } else if (battery.level >= .60 && battery.level < .80) {
+        batteryLevel.classList.add('fa-battery-3');
+      } else if (battery.level >= .80) {
+        batteryLevel.classList.add('fa-battery-4');
+      }
+    };
+
+  });
 };
